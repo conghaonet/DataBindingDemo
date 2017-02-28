01954cbe9f2f156cb6d0a858a5cce79c5faf0454
conghao <hao.cong@aliyun.com>
2017-02-28 17:43:02 +0800

    add commonTitle

---
+++ modulea/src/main/java/com/app2m/modulea/binding/model/CommonTitle.java
@@ -0,0 +1,20 @@
+package com.app2m.modulea.binding.model;
+
+import android.databinding.BaseObservable;
+
+/**
+ * Created by CongHao on 2017/2/28.
+ * E-mail: hao.cong@app2m.com
+ */
+
+public class CommonTitle extends BaseObservable {
+    private String title;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
